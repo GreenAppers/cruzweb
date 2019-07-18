008205acbe39f340b1aaa911b5883659b7688d22
GreenAppers <GreenAppers@gmail.com>
2019-07-18 16:20:33 -0700

    Add Delta Time to BlockWidget

--- web/main.dart
+++ web/main.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a MIT-style license that can be found in the LICENSE file.
 
 import 'dart:html';
-import 'dart:typed_data';
 
 import 'package:flutter_web/material.dart';
 import 'package:flutter_web_ui/ui.dart' as ui;
@@ -11,44 +10,16 @@ import 'package:scoped_model/scoped_model.dart';
 import 'package:sembast/sembast_memory.dart';
 
 import 'package:cruzawl/currency.dart';
-import 'package:cruzawl/cruz.dart';
 import 'package:cruzawl/network.dart';
+import 'package:cruzawl/preferences.dart';
 
 import 'package:cruzweb/cruzawl-ui/address.dart';
 import 'package:cruzweb/cruzawl-ui/block.dart';
 import 'package:cruzweb/cruzawl-ui/cruzbase.dart';
+import 'package:cruzweb/cruzawl-ui/model.dart';
+import 'package:cruzweb/cruzawl-ui/network.dart';
 import 'package:cruzweb/cruzawl-ui/transaction.dart';
-import 'package:cruzweb/cruzawl-ui/preferences.dart';
 import 'package:cruzweb/cruzawl-ui/ui.dart';
-
-class CruzWeb extends Model {
-  final CruzallPreferences preferences;
-  final Currency currency = Currency.fromJson('CRUZ');
-
-  CruzWeb(this.preferences) {
-    currency.network.autoReconnectSeconds = null;
-    currency.network.tipChanged = tipChanged;
-    currency.network.peerChanged = peerChanged;
-    currency.network
-        .addPeerWithSpec(
-            PeerPreference('SatoshiLocomoco', 'wallet.cruzbit.xyz', 'CRUZ', '',
-                debugPrint: debugPrint),
-            currency.genesisBlockId())
-        .connect();
-  }
-
-  void peerChanged() async {
-    if (currency.network.hasPeer)
-      (await currency.network.getPeer())
-          .filterAdd(CruzPublicKey(Uint8List(32)), (v) {});
-    notifyListeners();
-  }
-
-  void tipChanged() {
-    debugPrint('updated ${currency.network.tipHeight}');
-    notifyListeners();
-  }
-}
 
 class CruzWebLoading extends StatelessWidget {
   final Currency currency;
@@ -56,7 +27,7 @@ class CruzWebLoading extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    ScopedModel.of<CruzWeb>(context, rebuildOnChange: true);
+    ScopedModel.of<Cruzawl>(context, rebuildOnChange: true);
     final ThemeData theme = Theme.of(context);
     final TextStyle linkStyle = TextStyle(
       color: theme.accentColor,
@@ -72,27 +43,29 @@ class CruzWebLoading extends StatelessWidget {
           title: "Loading...");
     } else {
       String url = 'https' + currency.network.peerAddress.substring(3);
-      return SimpleScaffold(Container(
-          padding: EdgeInsets.all(32),
-          child: Column(
-            children: <Widget>[
-              Text('If this is a new browser session, visit:',
-                  style: labelTextStyle),
-              GestureDetector(
-                child: Text(url, style: linkStyle),
-                onTap: () => window.open(url, url),
-              ),
-              Text('Accept the certificate and refresh the page.',
-                  style: labelTextStyle),
-            ],
+      return SimpleScaffold(
+          Container(
+            padding: EdgeInsets.all(32),
+            child: Column(
+              children: <Widget>[
+                Text('If this is a new browser session, visit:',
+                    style: labelTextStyle),
+                GestureDetector(
+                  child: Text(url, style: linkStyle),
+                  onTap: () => window.open(url, url),
+                ),
+                Text('Accept the certificate and refresh the page.',
+                    style: labelTextStyle),
+              ],
+            ),
           ),
-        ), title: 'Error');
+          title: 'Error');
     }
   }
 }
 
 class CruzWebApp extends StatefulWidget {
-  final CruzWeb appState;
+  final Cruzawl appState;
   CruzWebApp(this.appState);
 
   @override
@@ -103,9 +76,9 @@ class CruzWebAppState extends State<CruzWebApp> {
   @override
   Widget build(BuildContext context) {
     final double maxWidth = 700;
-    final CruzWeb appState = widget.appState;
+    final Cruzawl appState = widget.appState;
     final ThemeData theme =
-      themes[appState.preferences.theme] ?? themes['deepOrange'];
+        themes[appState.preferences.theme] ?? themes['deepOrange'];
 
     return ScopedModel<SimpleScaffoldActions>(
       model: SimpleScaffoldActions(<Widget>[
@@ -114,6 +87,11 @@ class CruzWebAppState extends State<CruzWebApp> {
                 icon: Icon(Icons.settings),
                 text: 'Settings',
                 onSelected: () => window.location.hash = '/settings',
+              )
+              ..addItem(
+                icon: Icon(Icons.vpn_lock),
+                text: 'Network',
+                onSelected: () => window.location.hash = '/network',
               ))
             .build(
           icon: Icon(Icons.more_vert),
@@ -125,8 +103,10 @@ class CruzWebAppState extends State<CruzWebApp> {
         onGenerateRoute: (settings) {
           final String name = settings.name;
           const String address = '/address/',
+              addPeerUrl = '/addPeer',
               block = '/block/',
               height = '/height/',
+              networkUrl = '/network',
               settingsUrl = '/settings',
               tipUrl = '/tip',
               transaction = '/transaction/';
@@ -135,7 +115,7 @@ class CruzWebAppState extends State<CruzWebApp> {
             String addressText = name.substring(address.length);
             return MaterialPageRoute(
               settings: settings,
-              builder: (context) => ScopedModelDescendant<CruzWeb>(
+              builder: (context) => ScopedModelDescendant<Cruzawl>(
                   builder: (context, child, model) => addressText == 'cruzbase'
                       ? CruzbaseWidget(
                           appState.currency, appState.currency.network.tip)
@@ -145,7 +125,7 @@ class CruzWebAppState extends State<CruzWebApp> {
           } else if (name.startsWith(block))
             return MaterialPageRoute(
               settings: settings,
-              builder: (context) => ScopedModelDescendant<CruzWeb>(
+              builder: (context) => ScopedModelDescendant<Cruzawl>(
                 builder: (context, child, model) => BlockWidget(
                     appState.currency,
                     maxWidth: maxWidth,
@@ -156,7 +136,7 @@ class CruzWebAppState extends State<CruzWebApp> {
           else if (name.startsWith(height))
             return MaterialPageRoute(
               settings: settings,
-              builder: (context) => ScopedModelDescendant<CruzWeb>(
+              builder: (context) => ScopedModelDescendant<Cruzawl>(
                 builder: (context, child, model) => BlockWidget(
                     appState.currency,
                     maxWidth: maxWidth,
@@ -168,7 +148,7 @@ class CruzWebAppState extends State<CruzWebApp> {
           else if (name.startsWith(transaction))
             return MaterialPageRoute(
               settings: settings,
-              builder: (context) => ScopedModelDescendant<CruzWeb>(
+              builder: (context) => ScopedModelDescendant<Cruzawl>(
                 builder: (context, child, model) => TransactionWidget(
                     appState.currency, TransactionInfo(),
                     maxWidth: maxWidth,
@@ -181,21 +161,34 @@ class CruzWebAppState extends State<CruzWebApp> {
           else if (name.startsWith(settingsUrl))
             return MaterialPageRoute(
               settings: settings,
-              builder: (BuildContext context) => CruzWebSettings(appState, () => setState((){})),
+              builder: (BuildContext context) =>
+                  CruzWebSettings(appState, () => setState(() {})),
             );
+          else if (name.startsWith(networkUrl))
+            return MaterialPageRoute(
+                settings: settings,
+                builder: (BuildContext context) => SimpleScaffold(
+                    CruzawlNetworkSettings(),
+                    title: appState.currency.ticker + ' Network'));
+          else if (name.startsWith(addPeerUrl))
+            return MaterialPageRoute(
+                settings: settings,
+                builder: (BuildContext context) => SimpleScaffold(AddPeerWidget(), title: 'New Peer'));
           else if (name.startsWith(tipUrl))
             return MaterialPageRoute(
               settings: settings,
-              builder: (BuildContext context) => ScopedModelDescendant<CruzWeb>(
-                builder: (context, child, model) => BlockWidget(appState.currency,
-                    loadingWidget: loading, maxWidth: maxWidth),
+              builder: (BuildContext context) => ScopedModelDescendant<Cruzawl>(
+                builder: (context, child, model) => BlockWidget(
+                    appState.currency,
+                    loadingWidget: loading,
+                    maxWidth: maxWidth),
               ),
             );
 
           return MaterialPageRoute(
-            builder: (BuildContext context) => ScopedModelDescendant<CruzWeb>(
-              builder: (context, child, model) =>
-                  CruzbaseWidget(appState.currency, appState.currency.network.tip),
+            builder: (BuildContext context) => ScopedModelDescendant<Cruzawl>(
+              builder: (context, child, model) => CruzbaseWidget(
+                  appState.currency, appState.currency.network.tip),
             ),
           );
         },
@@ -205,7 +198,7 @@ class CruzWebAppState extends State<CruzWebApp> {
 }
 
 class CruzWebSettings extends StatefulWidget {
-  final CruzWeb appState;
+  final Cruzawl appState;
   final VoidCallback updateTheme;
   CruzWebSettings(this.appState, [this.updateTheme]);
 
@@ -216,32 +209,44 @@ class CruzWebSettings extends StatefulWidget {
 class _CruzWebSettingsState extends State<CruzWebSettings> {
   @override
   Widget build(BuildContext context) {
-    return SimpleScaffold(ListView(
-      padding: EdgeInsets.only(top: 20),
-      children: <Widget>[
-        ListTile(
-          leading: Icon(Icons.color_lens),
-          title: Text('Theme'),
-          trailing: DropdownButton<String>(
-            value: widget.appState.preferences.theme,
-            onChanged: (String val) {
-              widget.appState.preferences.theme = val;
-              if (widget.updateTheme != null) (widget.updateTheme)();
-            },
-            items: buildDropdownMenuItem(themes.keys.toList()),
-          ),
+    return SimpleScaffold(
+        ListView(
+          padding: EdgeInsets.only(top: 20),
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.color_lens),
+              title: Text('Theme'),
+              trailing: DropdownButton<String>(
+                value: widget.appState.preferences.theme,
+                onChanged: (String val) {
+                  widget.appState.preferences.theme = val;
+                  if (widget.updateTheme != null) (widget.updateTheme)();
+                },
+                items: buildDropdownMenuItem(themes.keys.toList()),
+              ),
+            ),
+          ],
         ),
-      ],
-    ), title: 'Settings');
+        title: 'Settings');
   }
 }
 
 void main() async {
   await ui.webOnlyInitializePlatform();
   debugPrint('Main ' + Uri.base.toString());
-  CruzWeb appState =
-    CruzWeb(CruzallPreferences(await databaseFactoryMemoryFs.openDatabase('settings.db')));
+
+  Cruzawl appState = Cruzawl(
+      CruzawlPreferences(
+          await databaseFactoryMemoryFs.openDatabase('settings.db')),
+      null);
+  appState.currency = Currency.fromJson('CRUZ');
+  appState
+      .addPeer(PeerPreference(
+          'SatoshiLocomoco', 'wallet.cruzbit.xyz', 'CRUZ', '',
+          debugPrint: debugPrint))
+      .connect();
+
   runApp(
-    ScopedModel<CruzWeb>(model: appState, child: CruzWebApp(appState)),
+    ScopedModel<Cruzawl>(model: appState, child: CruzWebApp(appState)),
   );
 }
